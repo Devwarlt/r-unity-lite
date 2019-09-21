276cf1050a96232a1f872ac933ddc09f5155d300
Devwarlt <devwarlt@loesoft.org>
2019-09-21 16:29:00 -0300

    MainScreen has been revised and improved
    Removed old layout of MainScree and custom font, because its not needed yet due too much customization. However, still pending to add a prefab for buttons for easy setup.

---
+++ Assets/Resources/Scripts/MainScreen.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts
+{
+    public class MainScreen : MonoBehaviour
+    {
+        [Header("Version Settings")]
+        public string versionTag;
+
+        public string versionKey;
+
+        private TextMeshProUGUI version;
+
+        private void GetGameObjects()
+        {
+            version = Utils.GetComponentByTag<TextMeshProUGUI>(versionTag);
+        }
+
+        private void InitializeGameObjects()
+        {
+            version.text = version.text.Replace(versionKey, Application.version);
+        }
+
+        private void Awake()
+        {
+            GetGameObjects();
+            InitializeGameObjects();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Utils.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Scripts
+{
+    public static class Utils
+    {
+        public static T GetComponentByTag<T>(string tag) => GameObject.FindWithTag(tag).GetComponent<T>();
+    }
+}
