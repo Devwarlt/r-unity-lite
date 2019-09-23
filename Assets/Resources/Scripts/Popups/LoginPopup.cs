0de0c7b8aa09fc4cb5f63ae1c4d9828ed18f6803
EpicQuackIV <49463775+EpicQuackIV@users.noreply.github.com>
2019-09-23 18:13:37 -0400

    improved login popup
    register popup still needs to be added
    also darkened color of disabled buttons

---
+++ Assets/Resources/Scripts/Popups/LoginPopup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPopup : MonoBehaviour
+{
+    [Header("Buttons")]
+    public Button cancelButton;
+    public Button loginButton;
+
+    void Awake()
+    {
+    	cancelButton.onClick.AddListener(() => Destroy(this.gameObject));
+    	//loginButton.onClick.AddListener(() => VerifyLogin());
+    }
+}
--- Assets/Resources/Scripts/Screens/Main/MainScreen.cs
+++ Assets/Resources/Scripts/Screens/Main/MainScreen.cs
@@ -20,6 +20,10 @@ namespace Assets.Resources.Scripts.Screens.Main
         public Button playButton;
         public Button quitButton;
 
+        [Header("Popups")]
+        public GameObject loginPopup;
+        public GameObject registerPopup;
+
         private TextMeshProUGUI version;
 
         private void GetGameObjects()
@@ -32,17 +36,42 @@ namespace Assets.Resources.Scripts.Screens.Main
             version.text = version.text.Replace(versionKey, Application.version);
 
             //playButton.onClick.AddListener(() => Utils.ChangeSceneAsync(GameScene.CharacterSelect, LoadSceneMode.Additive));
+            loginButton.onClick.AddListener(() => OpenPopup(loginPopup));
+            registerButton.onClick.AddListener(() => OpenPopup(registerPopup));
             serverButton.onClick.AddListener(() => Utils.ChangeSceneAsync(GameScene.Servers, LoadSceneMode.Additive));
             quitButton.interactable = Application.platform.HasQuitSupport();
             quitButton.onClick.AddListener(() => Application.Quit());
-            //registerButton.onClick.AddListener(() => open register gui);
-            //loginButton.onClick.AddListener(() => open login gui);
         }
 
         private void Awake()
         {
             GetGameObjects();
             InitializeGameObjects();
+        }
+
+        private void OpenPopup(GameObject popup)
+        {
+            GameObject go = Instantiate(popup, this.transform);
+            //go.onDisable.AddListener(() => EnableButtons());
+            //DisableButtons();
+        }
+
+        private void DisableButtons()
+        {
+            playButton.interactable = false;
+            serverButton.interactable = false;
+            quitButton.interactable = false;
+            loginButton.interactable = false;
+            registerButton.interactable = false;
+        }
+
+        private void EnableButtons()
+        {
+            playButton.interactable = true;
+            serverButton.interactable = true;
+            quitButton.interactable = Application.platform.HasQuitSupport();
+            loginButton.interactable = true;
+            registerButton.interactable = true;
         }
     }
 }
