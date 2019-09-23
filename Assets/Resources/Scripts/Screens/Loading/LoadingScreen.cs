142c2d50918b95cc840c474d60866ab461bfa664
EpicQuackIV <49463775+EpicQuackIV@users.noreply.github.com>
2019-09-23 02:30:34 -0400

    basic loading screen
    note: doesn't actually load anything

---
+++ Assets/Resources/Scripts/Screens/Loading/LoadingScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Assets.Resources.Scripts;
+
+public class LoadingScreen : MonoBehaviour
+{
+	public Button skipButton;
+
+    private void InitializeGameObjects()
+    {
+        skipButton.onClick.AddListener(() => OnLoadingComplete());
+    }
+
+    void Awake()
+    {
+    	InitializeGameObjects();
+    }
+
+    public void OnLoadingComplete()
+    {
+    	Utils.ChangeSceneAsync(GameScene.Main, LoadSceneMode.Single);
+    }
+}
