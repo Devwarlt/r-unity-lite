73eb82e6ce78aeb96dd2891c7c6f9a4d66181a26
EpicQuackIV <49463775+EpicQuackIV@users.noreply.github.com>
2019-09-24 01:44:47 -0400

    register popup is now closable

---
+++ Assets/Resources/Scripts/Popups/RegisterPopup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Assets.Resources.Scripts.Web.Handlers.app;
+using Assets.Resources.Scripts;
+
+public class RegisterPopup : MonoBehaviour
+{
+    [Header("Inputs")]
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public TMP_InputField confirmPassword;
+
+    [Header("Buttons")]
+    public Button cancelButton;
+    public Button registerButton;
+
+    [Header("Response Textbox")]
+    public TextMeshProUGUI responseBox;
+
+    private void InitializeGameObjects()
+    {
+    	responseBox.gameObject.SetActive(false);
+    	cancelButton.onClick.AddListener(() => Destroy(this.gameObject));
+    	registerButton.onClick.AddListener(() => VerifyRegister());
+    }
+
+    void Awake()
+    {
+    	InitializeGameObjects();
+    }
+
+    private void VerifyRegister()
+    {
+    	
+    }
+}
