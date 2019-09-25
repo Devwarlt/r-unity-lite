ac72f79496bc59e5684525c0c9e8ee24c3e8ebe5
ShotRotMG <shotrotmg@gmail.com>
2019-09-25 02:07:34 -0700

    Object storage and update account buttons at Mainscreen

---
+++ Assets/Resources/Scripts/Data/GOStorage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOStorage : MonoBehaviour
+{
+    public Dictionary<string, object> dict;
+
+    void Awake()
+    {
+        InitializeGameObjects();
+    }
+
+    private void InitializeGameObjects()
+    {
+        dict = new Dictionary<string, object>();
+    }
+
+    public object get(string name)
+    {
+        if (!dict.ContainsKey(name))
+            return null;
+        return dict[name];
+    }
+
+    public void set(string name, object obj)
+    {
+        if (dict.ContainsKey(name))
+            dict[name] = obj;
+        else
+            dict.Add(name, obj);
+    }
+}
