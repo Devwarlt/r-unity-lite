1a20c17d571a4805086e0fef90a821de8d0ca37e
ShotRotMG <shotrotmg@gmail.com>
2019-09-25 19:59:20 -0700

    Play Button blink effect

---
+++ Assets/Resources/Scripts/UI/Effects/SizeBlinkEffect.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeBlinkEffect : MonoBehaviour
+{
+    [Header("Values")]
+    public float maxSize;
+    public float minSize;
+    public float duration;
+
+    Vector3 max;
+    Vector3 min;
+
+    float startTime;
+    bool growing;
+
+    void Start()
+    {
+        startTime = Time.time;
+        growing = true;
+        max = new Vector3(maxSize, maxSize, maxSize);
+        min = new Vector3(minSize, minSize, minSize);
+    }
+
+    void Update()
+    {
+        var scaleSize = transform.localScale.y;
+
+        if (scaleSize >= max.y)
+        {
+            growing = false;
+            startTime = (float)(Time.time - .01);
+        }
+
+        if (scaleSize <= min.y)
+        {
+            growing = true;
+            startTime = (float)(Time.time - .01);
+        }
+
+        if (growing)
+            grow();
+        else
+            shrink();
+    }
+
+    void grow()
+    {
+        var transition = (Time.time - startTime) / duration;
+        transform.localScale = Vector3.Lerp(min, max, transition);
+    }
+    
+    void shrink()
+    {
+        var transition = (Time.time - startTime) / duration;
+        transform.localScale = Vector3.Lerp(max, min, transition);
+    }
+}
