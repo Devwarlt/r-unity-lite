676beeeb06a1b7f8b01d18a3eb9669650ad66d33
ShotRotMG <shotrotmg@gmail.com>
2019-09-25 21:32:23 -0700

    Switch back to Priest's server ip

--- Assets/Resources/Scripts/Data/AppEngine.cs
+++ Assets/Resources/Scripts/Data/AppEngine.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public static class AppEngine
 {
     public static GameWebProtocol webServerProtocol => GameWebProtocol.HTTP;
-    public static string webServerIPAddress => "play.shotsrealm.com";
+    public static string webServerIPAddress => "54.39.227.169";
     public static int webServerPort => 8080;
 
-    public static string getUrl() => $"{webServerProtocol.ToString().ToLower()}://{webServerIPAddress}";//:{webServerPort.ToString()}
+    public static string getUrl() => $"{webServerProtocol.ToString().ToLower()}://{webServerIPAddress}:{webServerPort.ToString()}";
 
     public static string getBuild() => "Shot's Realm #{VERSION}".Replace("{VERSION}", Application.version);
 }
