b3844b0bb3eead9efa96e3ef11fa1f760160e108
Devwarlt <devwarlt@loesoft.org>
2019-11-18 19:19:54 -0200

    Working on sprite splitter

---
+++ Assets/Editor/ObjectBuilderEditor.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Core.Utils;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TileSplitter))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var myScript = (TileSplitter)target;
+
+        if (GUILayout.Button("Split Tiles")) myScript.SplitTiles();
+    }
+}
